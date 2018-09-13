0749f8014bcb166da69b35144b69eb85a26f7fb4
Dusan <mac@n191-p202.eduroam.kth.se>
2018-09-13 11:49:03 +0200

    EM och algoritmen för antal clusters

--- em_algo.py
+++ em_algo.py
@@ -1,6 +1,7 @@
 from sklearn.mixture import GaussianMixture
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.cluster import DBSCAN
 
 def data_gen(num_samples, num_clusters, dim = 2):
 
@@ -35,7 +36,7 @@ def data_gen(num_samples, num_clusters, dim = 2):
 		data[data_offset:data_offset + samples_per_cluster] = np.random.multivariate_normal(mean, cov, samples_per_cluster)
 		x_val = data[data_offset:data_offset + samples_per_cluster].T[0]
 		y_val = data[data_offset:data_offset + samples_per_cluster].T[1]
-		plt.plot(x_val, y_val, plot_colors[i])
+		#plt.plot(x_val, y_val, plot_colors[i])
 		data_offset += samples_per_cluster
 
 	x_values = data.T[0]
@@ -43,7 +44,29 @@ def data_gen(num_samples, num_clusters, dim = 2):
 
 
 	#plt.plot(x_values, y_values, 'ro')
-	plt.show()
-	plt.close()
+	#plt.show()
+	#plt.close()
+	return data
 
-data_gen(num_samples=100, num_clusters=5)
+
+
+def expectation_maximization(X):
+	model = GaussianMixture()
+	db = DBSCAN(eps=1, min_samples=10).fit(X)
+	#core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+	#core_samples_mask[db.core_sample_indices_] = True
+	labels = db.labels_
+	n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+	#print(db.get_params())
+	print("Number of clusters",len(set(labels)))
+	print("Number of clusters without noise", n_clusters_)
+	num_clusters = len(set(labels))
+	for i in range(num_clusters):
+		model.set_params(n_components=i+1)
+		model.fit(X)
+		#print(model.get_params())
+		print(model.score(X))
+
+
+X = data_gen(num_samples=100, num_clusters=5)
+expectation_maximization(X)

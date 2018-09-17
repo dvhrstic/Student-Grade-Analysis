f985ca49a2482b4794cfc63f80c640a78e6368ee
niklasembretsen <niklasembretsen@gmail.com>
2018-09-17 22:20:12 +0200

    Fixing map representation + refactoring and documenting the functions

--- som.py
+++ som.py
@@ -4,110 +4,158 @@ import matplotlib
 import matplotlib.pyplot as plt
 np.random.seed(9)
 
+""" TODO:
+	- fix decaying learning learning
+	- fix decaying radius
+"""
+
 class SOMNetwork:
 	def __init__(self, layers_dim,cyclic = False, mp = False):
 		super(SOMNetwork, self).__init__()
-		self.layers = len(layers_dim)-1
-		self.learning_rate = 0.01
+		self.learning_rate = 0.002
 		self.W = self.generate_weight(layers_dim)
-		self.step_size = 0.2
-		self.cyclic = cyclic
-		self.mp = mp
-		if(cyclic):
-			self.get_neighbourhood = self.get_neighbourhood_cyclic
-		elif(mp):
-			self.get_neighbourhood = self.get_neighbourhood_mp
-		else:
-			self.get_neighbourhood = self.get_neighbourhood
 
 	def generate_weight(self,layers_dim):
-		W = np.random.uniform(0,1,layers_dim).T
+		"""Initialize the SOM map
+			Parameters
+			----------
+			layers_dim : array
+				contains the dimensions of the map
+				[nrRows, nrCols, inputDim]
+
+			Returns
+			-------
+			W : nrROws x nrCols x inputDim array
+				with uniformly sampled values between 0 and 1
+		"""
+		nr_rows = layers_dim[0]
+		nr_cols = layers_dim[1]
+		input_dim = layers_dim[2]
+		W = np.random.uniform(0,1,(nr_rows, nr_cols, input_dim))
 		return W
 
-	def get_neighbourhood(self,i, size = 50):
-		neighbourhood = np.array(list(range(i-size, i))+list(range(i, i+size+1)))
-		if(neighbourhood[0] < 0):
-			neighbourhood = neighbourhood[np.where(neighbourhood >= 0)[0]]
-		if(neighbourhood[len(neighbourhood)-1] > len(self.W)-1):
-		 	neighbourhood = neighbourhood[np.where(neighbourhood < len(self.W))[0]]
+	def get_neighbourhood(self, winner, radius):
+		""" Get the neigbourhood of winning node within
+		specified radius
 
-		return neighbourhood
+		Parameters
+		----------
+		winner : array 
+			coordinates of winning node
+			[x, y]
+		radius : integer
+			radius of neighbourhood
 
-	def get_neighbourhood_cyclic(self, i, size = 2):
-		neighbourhood = np.array(list(range(i-size, i))+list(range(i, i+size+1)))
-		neighbourhood = neighbourhood % len(self.W)
-		return neighbourhood
+		Returns
+		-------
+		neighbourhood : array
+			array with coordinates of all neighbours
+			to the winning node
+		"""
+		nr_rows = self.W.shape[0]
+		nr_cols = self.W.shape[1]
 
-	def get_neighbourhood_mp(self, idx, size = 2):
-		count = 0
-		neighbourhood = np.zeros((10,10), dtype='object')
-		rowNumber = 0
-		columnNumber = 0
-		radius = size
-		for i in range(5):
-			for j in range(5):
-				neighbourhood[i][j] = (count, self.W[count])
-				if(idx == count):
-					rowNumber = i + 1
-					columnNumber = j + 1
-				count += 1
+		row_span = np.arange(winner[0] - radius, winner[0] + radius + 1)
+		col_span = np.arange(winner[1] - radius, winner[1] + radius + 1)
 
-		temp = np.zeros((10,10), dtype = 'object')
-		for i in range(rowNumber-1-radius,rowNumber+radius):
-			for j in range(columnNumber-1-radius, columnNumber+radius):
-				if(i >= 0 and i < len(neighbourhood) and j >= 0 and j < len(neighbourhood[0])):
-					temp[i][j] = neighbourhood[i][j]
+		neighbourhood = []
+		for i in range((2*radius) + 1):
+			for j in range((2*radius) + 1):
+				if((row_span[i] > (nr_rows - 1)) or (row_span[i] < 0) \
+					or (col_span[j] > (nr_cols - 1)) or (col_span[j] < 0)):
+					continue
+				else: 
+					neighbourhood.append([row_span[i], col_span[j]])
 
+		return neighbourhood 
 
-		neighbs = []
-		for i in range(len(temp)):
-			for j in range(len(temp[0])):
-				if(temp[i][j] != 0):
-					neighbs.append(temp[i][j][0])
+	def get_winner(self, x, epochs, epoch):
+		""" Get the winning node for an input
 
-		return neighbs	 	
+		Parameters
+		----------
+		x : 1 x m array
+			one input data point
+		epochs : integer
+			total number of epochs TODO: move into object instead
+		epoch : integer
+			the current epoch
+		
+		Returns
+		-------
+		winner : array
+			array containing coordinates of the winning node
+		radius : integer
+			the radius of the neighbourhood
+		"""
 
-
-	def train(self, X, epochs):
-		for epoch in range(epochs):
-			for i,x in enumerate(X):
-				winner = 0
-				winner_dist = 999999999
-				for j, w in enumerate(self.W):
-					temp_distance = np.subtract(x,w)
-					distance = np.matmul(temp_distance.T,temp_distance)
-
-					if(distance < winner_dist):
-						winner = j
-						winner_dist = distance
-				
-				if(self.cyclic != True and self.mp != True):
-					neighbourhood = self.get_neighbourhood(winner, epochs-epoch)
-				else:
-					treshhold = epochs / 3
-					size = 3
-					if(epoch > treshhold and epoch < 2*treshhold):
-						size = 2
-					elif(epoch > 2*treshhold):
-						size = 0
-
-					neighbourhood = self.get_neighbourhood(winner, size)
-					
-				for neighbour in neighbourhood:
-					self.W[neighbour] += self.step_size*np.subtract(x, self.W[neighbour])
-
-	def predict(self, X):
-		winners = np.zeros(len(X), dtype='int')
-		for i,x in enumerate(X):
-			winner = 0
-			winner_dist = 999999999
-			for j, w in enumerate(self.W):
-				temp_distance = np.subtract(x,w)
+		winner = []
+		winner_dist = 999999999
+		for i in range(len(self.W)):
+			for j in range(len(self.W[0])):
+				temp_distance = np.subtract(x,self.W[i][j])
 				distance = np.matmul(temp_distance.T,temp_distance)
 
 				if(distance < winner_dist):
-					winner = j
+					winner = [i, j]
 					winner_dist = distance
+		
+		treshhold = epochs / 3
+		radius = 3
+		if(epoch > treshhold and epoch < 2*treshhold):
+			radius = 2
+		elif(epoch > 2*treshhold):
+			radius = 0 
+
+		return winner, radius
+
+
+	def train(self, X, epochs):
+		""" Update the SOM map to move the nodes towards
+		the input data
+
+		Parameters
+		----------
+		X : n x m array
+			the input data set
+		epochs : integer
+			number of epochs used for training TODO: move into SOM object instead
+		"""
+		for epoch in range(epochs):
+			for x in X:
+				winner, radius = self.get_winner(x, epochs, epoch)
+				neighbourhood = self.get_neighbourhood(winner, radius)
+					
+				for n in neighbourhood:
+					self.W[n[0]][n[1]] += self.learning_rate*np.subtract(x, self.W[n[0]][n[1]])
+
+	def predict(self, X):
+		""" Predict the winning node for each data point after
+		training is done (for visualization purposes)
+
+		Parameters
+		----------
+		X : n x m array
+			the input data set
+
+		Returns
+		-------
+		winners : n x 2 array
+			the coordinates of the winning node
+			for each input
+		"""
+		winners = np.zeros((len(X), 2), dtype='int')
+		for i,x in enumerate(X):
+			winner = 0
+			winner_dist = 999999999
+			for j in range(len(self.W)):
+				for k in range(len(self.W[0])): 
+					temp_distance = np.subtract(x, self.W[j][k])
+					distance = np.matmul(temp_distance.T,temp_distance)
+
+					if(distance < winner_dist):
+						winner = [j, k]
+						winner_dist = distance
 
 			winners[i] = winner
 
--- som_main.py
+++ som_main.py
@@ -7,17 +7,13 @@ from collections import Counter
 def votes():
 	
 	raw_data = np.random.randint(0, 255, (3, 100)).T
-	layer_dim = [3, 25]
+	layer_dim = [5,5,3]
 	som = SOM.SOMNetwork(layer_dim, mp=True)
-	som.train(raw_data, 100)
-	#result = som.predict(raw_data)
-	grid = np.zeros((5, 5, 3))
-	temp = 0
-	for i in range(len(grid)):
-		for j in range(len(grid[0])):
-			grid[i][j] = som.W[temp]
-			temp += 1
-	plt.imshow(grid / 255)
+
+	som.train(raw_data, 5000)
+	result = som.predict(raw_data)
+
+	plt.imshow(som.W / 255)
 	plt.show()
 	#hejhej
 

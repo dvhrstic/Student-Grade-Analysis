548c32e44c82b42a7ef3633c430d5ed71a860ed0
niklasembretsen <niklasembretsen@gmail.com>
2018-09-17 15:33:17 +0200

    new SOM
    A class for the SOM-network

--- som.py
+++ som.py
@@ -1,20 +1,122 @@
 import numpy as np
+import random
+import matplotlib
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import somoclu
+np.random.seed(9)
 
-c1 = np.random.rand(50, 3)/5
-c2 = (0.6, 0.1, 0.05) + np.random.rand(50, 3)/5
-c3 = (0.4, 0.1, 0.7) + np.random.rand(50, 3)/5
-data = np.float32(np.concatenate((c1, c2, c3)))
-colors = ["red"] * 50
-colors.extend(["green"] * 50)
-colors.extend(["blue"] * 50)
-fig = plt.figure()
-ax = Axes3D(fig)
-ax.scatter(data[:, 0], data[:, 1], data[:, 2], c=colors)
-labels = range(150)
+class SOMNetwork:
+	def __init__(self, layers_dim,cyclic = False, mp = False):
+		super(SOMNetwork, self).__init__()
+		self.layers = len(layers_dim)-1
+		self.learning_rate = 0.01
+		self.W = self.generate_weight(layers_dim)
+		self.step_size = 0.2
+		self.cyclic = cyclic
+		self.mp = mp
+		if(cyclic):
+			self.get_neighbourhood = self.get_neighbourhood_cyclic
+		elif(mp):
+			self.get_neighbourhood = self.get_neighbourhood_mp
+		else:
+			self.get_neighbourhood = self.get_neighbourhood
 
-n_rows, n_columns = 100, 160
-som = somoclu.Somoclu(n_columns, n_rows, compactsupport=False)
-#som.train(data)
+	def generate_weight(self,layers_dim):
+		W = np.random.uniform(0,1,layers_dim).T
+		return W
+
+	def get_neighbourhood(self,i, size = 50):
+		neighbourhood = np.array(list(range(i-size, i))+list(range(i, i+size+1)))
+		if(neighbourhood[0] < 0):
+			neighbourhood = neighbourhood[np.where(neighbourhood >= 0)[0]]
+		if(neighbourhood[len(neighbourhood)-1] > len(self.W)-1):
+		 	neighbourhood = neighbourhood[np.where(neighbourhood < len(self.W))[0]]
+
+		return neighbourhood
+
+	def get_neighbourhood_cyclic(self, i, size = 2):
+		neighbourhood = np.array(list(range(i-size, i))+list(range(i, i+size+1)))
+		neighbourhood = neighbourhood % len(self.W)
+		return neighbourhood
+
+	def get_neighbourhood_mp(self, idx, size = 2):
+		count = 0
+		neighbourhood = np.zeros((10,10), dtype='object')
+		rowNumber = 0
+		columnNumber = 0
+		radius = size
+		for i in range(10):
+			for j in range(10):
+				neighbourhood[i][j] = (count, self.W[count])
+				if(idx == count):
+					rowNumber = i + 1
+					columnNumber = j + 1
+				count += 1
+
+		temp = np.zeros((10,10), dtype = 'object')
+		for i in range(rowNumber-1-radius,rowNumber+radius):
+			for j in range(columnNumber-1-radius, columnNumber+radius):
+				if(i >= 0 and i < len(neighbourhood) and j >= 0 and j < len(neighbourhood[0])):
+					temp[i][j] = neighbourhood[i][j]
+
+
+		neighbs = []
+		for i in range(len(temp)):
+			for j in range(len(temp[0])):
+				if(temp[i][j] != 0):
+					neighbs.append(temp[i][j][0])
+
+		return neighbs	 	
+
+
+	def train(self, X, epochs):
+		for epoch in range(epochs):
+			# if(epoch%100 == 0):
+			# 	plt.plot(self.W.T[0], self.W.T[1],'go')
+			# 	plt.plot(X.T[0], X.T[1], 'bx')
+			# 	plt.plot(self.W.T[0], self.W.T[1],'ro')
+			# 	plt.plot(self.W.T[0], self.W.T[1],'-')
+			# 	plt.savefig('images/som'+str(epoch)+'.png')
+			# 	plt.close()
+			for i,x in enumerate(X):
+				winner = 0
+				winner_dist = 999999999
+				for j, w in enumerate(self.W):
+					temp_distance = np.subtract(x,w)
+					distance = np.matmul(temp_distance.T,temp_distance)
+
+					if(distance < winner_dist):
+						winner = j
+						winner_dist = distance
+				
+				if(self.cyclic != True and self.mp != True):
+					neighbourhood = self.get_neighbourhood(winner, epochs-epoch)
+				else:
+					treshhold = epochs / 3
+					size = 3
+					if(epoch > treshhold and epoch < 2*treshhold):
+						size = 2
+					elif(epoch > 2*treshhold):
+						size = 0
+
+					neighbourhood = self.get_neighbourhood(winner, size)
+					
+				for neighbour in neighbourhood:
+					self.W[neighbour] += self.step_size*np.subtract(x, self.W[neighbour])
+
+	def predict(self, X):
+		winners = np.zeros(len(X), dtype='int')
+		for i,x in enumerate(X):
+			winner = 0
+			winner_dist = 999999999
+			for j, w in enumerate(self.W):
+				temp_distance = np.subtract(x,w)
+				distance = np.matmul(temp_distance.T,temp_distance)
+
+				if(distance < winner_dist):
+					winner = j
+					winner_dist = distance
+
+			winners[i] = winner
+
+		return winners
+	

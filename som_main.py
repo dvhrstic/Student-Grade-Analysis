a65924efc1726452b4d7bc3cc85a7b5a36847ffc
niklasembretsen <niklasembretsen@gmail.com>
2018-09-18 17:38:52 +0200

    Test

--- som_main.py
+++ som_main.py
@@ -6,12 +6,12 @@ from collections import Counter
 
 def test():
 	
-	raw_data = np.random.randint(0, 255, (100, 3))
+	raw_data = np.random.randint(0, 255, (500, 3))
 	#normalize the data
 	raw_data = raw_data / np.max(raw_data)
 
-	layer_dim = [5,5,3]
-	som = SOM.SOMNetwork(layer_dim, epochs=4000)
+	layer_dim = [10,10,3]
+	som = SOM.SOMNetwork(layer_dim, epochs=3000)
 
 	som.train(raw_data)
 	result = som.predict(raw_data)

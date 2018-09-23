c08b3ab4b57c732f878339c5d21e8f8e81a25a4d
Dusan <mac@n138-p37.eduroam.kth.se>
2018-09-23 15:15:00 +0200

    Renamed to appropriate name

---
+++ km_algo.py
@@ -0,0 +1,85 @@
+from sklearn.cluster import KMeans
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import DBSCAN
+
+def data_gen(num_samples, num_clusters, dim = 2):
+
+	data = np.zeros((num_samples, dim))
+
+	means = np.zeros((num_clusters, dim))
+
+	samples_per_cluster = num_samples//num_clusters
+
+	plot_colors = ['#E69F00', '#56B4E9', '#F0E442', '#009E73', '#D55E00', '#D57800']
+
+	horizontal = 5
+	vertical = 5
+
+	hor_step = 5
+	ver_step = -5
+
+	for i in range(len(means)):
+		if (i % 2 == 0):
+			#move horizontally
+			means[i] = np.array([horizontal, vertical])
+			horizontal += hor_step
+		else:
+			#move vertically
+			means[i] = np.array([horizontal, vertical])
+			vertical += (-1)*ver_step
+			ver_step *= -1
+
+	data_offset = 0
+	for i, mean in enumerate(means):
+		cov = np.random.random((dim, dim))
+		data[data_offset:data_offset + samples_per_cluster] = np.random.multivariate_normal(mean, cov, samples_per_cluster)
+		x_val = data[data_offset:data_offset + samples_per_cluster].T[0]
+		y_val = data[data_offset:data_offset + samples_per_cluster].T[1]
+		plt.plot(x_val, y_val, plot_colors[i],linestyle="None",marker='o')
+		data_offset += samples_per_cluster
+
+	x_values = data.T[0]
+	y_values = data.T[1]
+
+
+	#plt.plot(x_values, y_values, 'ro')
+	plt.show()
+	plt.close()
+	return data
+
+
+
+def kmeans_DBSCAN(X):
+	db = DBSCAN(eps=1, min_samples=50).fit(X)
+	labels = db.labels_
+	n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+	print("---DBSCAN---")
+	print("---Number of clusters: ",len(set(labels)))
+	print("---Number of clusters without noise: ", n_clusters_)
+
+
+def elbow_method(X, model, num_clusters):
+	k_clusters = np.arange(1, num_clusters)
+	scores = []
+	for k in k_clusters:
+		model.set_params(n_clusters=k)
+		model.fit(X)
+		scores.append(model.score(X))
+	plt.plot(k_clusters, scores)
+	plt.title('Elbow Method')
+	plt.xlabel('Number of clusters')
+	plt.ylabel('Scores')
+	plt.show()
+
+def main():
+	# Data generation
+	X = data_gen(num_samples=600, num_clusters=6)	
+	# K-means initialization
+	model = KMeans()
+	# Getting the optimal number of clusters with DBSCAN method
+	kmeans_DBSCAN(X)
+	# Elbow method for different number of clusters
+	elbow_method(X,model, num_clusters=10)
+
+main()

d5f5a066e2d09b84fcd5a713c9cb7fa1a0aa18c6
niklasembretsen <niklasembretsen@gmail.com>
2018-09-24 09:26:57 +0200

    Test plot function for SOM network

--- som_main.py
+++ som_main.py
@@ -12,14 +12,20 @@ def test():
 	#normalize the data
 	#raw_data = raw_data / np.max(raw_data)
 
-	layer_dim = [20,20,X.shape[1]]
-	som = SOM.SOMNetwork(layer_dim, epochs=1)
+	layer_dim = [40,40,X.shape[1]]
+	som = SOM.SOMNetwork(layer_dim, epochs=10)
 
 	som.train(X)
 	result = som.predict(X)
 
 	print(result)
 	print(result.shape)
+
+	x_data = result[:,1]
+	y_data = result[:,2]
+
+	plt.plot(x_data, y_data, 'ro', linestyle="none")
+	plt.show()
 
 	# plt.imshow(som.W)
 	# plt.show()

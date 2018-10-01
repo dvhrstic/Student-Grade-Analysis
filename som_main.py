8534b15cf482b2344b2276eaacc227ea5e404903
niklasembretsen <niklasembretsen@gmail.com>
2018-10-01 12:28:39 +0200

    Added new functionality for using seaborn heatmap

--- som_main.py
+++ som_main.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 import som as SOM
+import seaborn as sns; sns.set()
 
 def test():
 
@@ -12,8 +13,8 @@ def test():
 	#normalize the data
 	#raw_data = raw_data / np.max(raw_data)
 
-	layer_dim = [40,40,X.shape[1]]
-	som = SOM.SOMNetwork(layer_dim, epochs=10)
+	layer_dim = [30,30,X.shape[1]]
+	som = SOM.SOMNetwork(layer_dim, epochs=500)
 
 	som.train(X)
 	result = som.predict(X)
@@ -23,12 +24,29 @@ def test():
 
 	x_data = result[:,1]
 	y_data = result[:,2]
-
-	plt.plot(x_data, y_data, 'ro', linestyle="none")
-	plt.show()
-
-	# plt.imshow(som.W)
+	# plt.plot(x_data, y_data, 'ro', linestyle="none")
 	# plt.show()
-	#hejhej
+
+	grid = create_grid(result, layer_dim)
+
+	ax = sns.heatmap(grid, annot=False, fmt="d")
+	plt.savefig("noNum.png")
+	plt.close()
+
+	ax = sns.heatmap(grid, annot=True, fmt="d")
+	plt.savefig("withNum.png")
+	plt.close()
+
+def create_grid(data, dim):
+	rows = dim[0]
+	cols = dim[1]
+
+	grid = np.zeros((rows, cols), dtype='int')
+
+	for d in data:
+		grid[d[1]][d[2]] += 1
+
+	return grid
+	print(grid)
 
 test()
